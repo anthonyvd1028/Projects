660950b5936e230e31d8ca8c045e8567143449b7
anthonyvd1028 <avd8@njit.edu>
2024-09-19 19:02:31 -0400

    Added author name and removed echo command

--- Projects/DownloadCleaner/DownloadCleaner.sh
+++ Projects/DownloadCleaner/DownloadCleaner.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #Script that deletes files and folders in Downloads that are older then 2 weeks
+#Author: Anthony DiTaranto
+
 cd ~/Downloads
 
 now=$(date +%s)
@@ -8,7 +10,6 @@ now=$(date +%s)
 for file in *; do 
     if [ $((now - $(stat -c %Y "$file"))) -gt $((2 * 7 * 24 * 3600)) ]; then
         rm -r "$file"
-        echo "Deleted: $file"
     fi
 done
 echo "Done"
